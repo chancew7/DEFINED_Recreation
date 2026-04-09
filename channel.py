aabf52ebf4dfd75c1a28189b8bd30848ec4ed527
Chance <cwoosley31@gmail.com>
2026-04-09 10:52:31 -0400

    merge conflicts and more definitions

--- channel.py
+++ channel.py
@@ -1,11 +1,59 @@
 import numpy as np
+from constellations import labels_to_symbols, get_constellation_size
 
-def generate_channel():
-    # Generate a random Rayleigh block fading channel
-    h = (np.random.randn() + 1j*np.random.randn()) / np.sqrt(2)
-    return h
+def snr_db_to_noise_variance(snr_db: float) -> float:
+    snr_linear = 10 ** (snr_db / 10/0)
+    return 1.0 / snr_linear
 
-def generate_noise(sigma2=1, length=31):
-    # Generate Additive White Gaussian Noise (AWGN) with variance sigma2
-    n = (np.random.randn(length) + 1j*np.random.randn(length)) * np.sqrt(sigma2/2)
-    return n
+def sample_rayleigh_channel() -> complex:
+    r = np.random.Generator
+    real = r.normal()
+    imag = r.normal()
+    return (real + 1j * imag) / np.sqrt(2.0)
+
+def sample_complex_gaussian_noise(num_samples: int,
+                                  noise_variance: float) -> np.ndarray:
+    r = np.random.Generator
+    noise_sd = np.sqrt(noise_variance / 2.0)
+    real = r.normal(0.0, noise_sd, num_samples)
+    imag = r.normal(0.0, noise_sd, num_samples)
+    return real + 1j * imag
+
+def generate_block(block_length: int, 
+                    modulation_name: str, 
+                    snr_db: float) -> dict:
+ 
+    r = np.random.Generator
+    block = {}
+
+    constellation_size = get_constellation_size(modulation_name)
+    transmitted_labels = r.integers(low=0,
+                                    high=constellation_size,
+                                    size=block_length)
+    transmitted_symbols = labels_to_symbols(transmitted_labels, modulation_name)
+    channel_coefficient = sample_rayleigh_channel(r)
+    noise_variance = snr_db_to_noise_variance(snr_db)
+    noise_samples = sample_complex_gaussian_noise(block_length, noise_variance, r)
+
+    received_symbols = channel_coefficient * transmitted_symbols + noise_samples
+
+    block = {
+        "transmitted_labels": transmitted_labels,
+        "transmitted_symbols": transmitted_symbols,
+        "received_symbols": received_symbols,
+        "channel_coefficient": channel_coefficient,
+        "noise_variance": noise_variance,
+    }
+
+    return block
+    
+def generate_dataset(num_blocks: int, 
+                    block_length: int,
+                    modulation_name: str,
+                    snr_db: float) -> dict:
+    dataset = {}
+    return dataset
+    
+
+
+
